e259839ec048e00005fb24f56e2554a4632ac89b
Luis Rivera <chubakueno@gmail.com>
2017-08-20 15:11:38 -0500

    estilos

--- app.js
+++ app.js
@@ -260,14 +260,4 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
     flexDirection: 'row',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
